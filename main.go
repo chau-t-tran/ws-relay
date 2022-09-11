daf2d0cde3f22ffb3b34af0f8d11cf6d47434ff1
chau-t-tran <chau.t.tran.us@gmail.com>
2022-09-11 18:21:34 -0400

    Moved server init to package

--- main.go
+++ main.go
@@ -1,25 +1,8 @@
 package main
 
-import (
-	"math/rand"
-	"time"
-
-	"github.com/chau-t-tran/ws-relay/routes"
-	"github.com/chau-t-tran/ws-relay/templates"
-	"github.com/labstack/echo/v4"
-)
+import "github.com/chau-t-tran/ws-relay/server"
 
 func main() {
-	e := echo.New()
-
-	/* ------Pseudorand Seed----- */
-	rand.Seed(int64(time.Now().UTC().UnixNano()))
-
-	/* ----------Registry-------- */
-	e.Renderer = templates.Renderer
-
-	/* ----------Routes---------- */
-	routes.SetupRootRoutes(e)
-
+	e := server.GetServer()
 	e.Logger.Fatal(e.Start(":1323"))
 }
