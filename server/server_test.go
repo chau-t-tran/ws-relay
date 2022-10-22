0f004632a360ccba481c0183fef18247b11f6b00
Chau Tran <chau.t.tran.us@gmail.com>
2022-10-22 06:16:13 -0400

    Integration tests

--- server/server_test.go
+++ server/server_test.go
@@ -1,11 +1,17 @@
 package server
 
 import (
+	"bytes"
+	"encoding/json"
 	"fmt"
+	"math/rand"
 	"net/http"
 	"testing"
 	"time"
 
+	"github.com/chau-t-tran/ws-relay/constants"
+	"github.com/chau-t-tran/ws-relay/utils"
+	"github.com/chau-t-tran/ws-relay/ws_manager"
 	"github.com/labstack/echo/v4"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/suite"
@@ -13,16 +19,25 @@ import (
 
 type RootTestSuite struct {
 	suite.Suite
-	e                     *echo.Echo
-	PORT                  int
-	RequestTimeoutSeconds int
+	seed       int
+	e          *echo.Echo
+	PORT       int
+	httpClient http.Client
 }
 
 /*-------------------Setups/Teardowns-------------------*/
 
 func (suite *RootTestSuite) SetupSuite() {
 	suite.PORT = 9000
-	suite.RequestTimeoutSeconds = 5
+	suite.seed = constants.KEY_TEST_SEED
+	suite.e = GetServer()
+	suite.httpClient = http.Client{
+		Timeout: 5 * time.Second,
+	}
+	go func() {
+		suite.e.Logger.Fatal(suite.e.Start(fmt.Sprintf(":%d", suite.PORT)))
+	}()
+	time.Sleep(5 * time.Second)
 }
 
 func (suite *RootTestSuite) TeardownSuite() {
@@ -34,23 +49,43 @@ func (suite *RootTestSuite) TeardownSuite() {
 /*-------------------Tests------------------------------*/
 
 func (suite *RootTestSuite) TestServerIsOnline() {
-	suite.e = GetServer()
-	go func() {
-		suite.e.Logger.Fatal(suite.e.Start(fmt.Sprintf(":%d", suite.PORT)))
-	}()
+	url := fmt.Sprintf("http://localhost:%d/health", suite.PORT)
+	resp, err := suite.httpClient.Get(url)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), http.StatusOK, resp.StatusCode)
+}
 
-	got200 := false
+func (suite *RootTestSuite) TestLandingPage() {
+	url := fmt.Sprintf("http://localhost:%d/", suite.PORT)
+	resp, err := suite.httpClient.Get(url)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), http.StatusOK, resp.StatusCode)
 
-	for i := 0; i < suite.RequestTimeoutSeconds; i++ {
-		resp, _ := http.Get(fmt.Sprintf("http://localhost:%d/health", suite.PORT))
-		if resp != nil {
-			got200 = resp.StatusCode == 200
-			break
-		}
-		time.Sleep(1 * time.Second)
-	}
+	contentTypeArray, ok := resp.Header["Content-Type"]
+	assert.True(suite.T(), ok)
 
-	assert.True(suite.T(), got200)
+	contentType := contentTypeArray[0]
+	assert.Equal(suite.T(), "text/html; charset=UTF-8", contentType)
+}
+
+func (suite *RootTestSuite) TestRegistrationCall() {
+	rand.Seed(int64(suite.seed))
+	sessionKey := utils.RandomKey()
+	rand.Seed(int64(suite.seed))
+
+	respTarget := ws_manager.SessionRegResponse{}
+	url := fmt.Sprintf("http://localhost:%d/register", suite.PORT)
+	contentType := "application/json"
+	jsonBody := []byte(`{}`)
+	bodyReader := bytes.NewReader(jsonBody)
+
+	resp, err := suite.httpClient.Post(url, contentType, bodyReader)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), http.StatusCreated, resp.StatusCode)
+
+	err = json.NewDecoder(resp.Body).Decode(&respTarget)
+	assert.NoError(suite.T(), err)
+	assert.Equal(suite.T(), respTarget.SessionKey, sessionKey)
 }
 
 /*-------------------Runner-----------------------------*/
